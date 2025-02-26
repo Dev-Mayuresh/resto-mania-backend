b56f5ae4d72c965e0be317603e95d31abb5f6b51
shantanushok <159703391+shantanushok@users.noreply.github.com>
2025-02-27 01:01:50 +0530

    Update billRequestSockets.js

--- src/sockets/billRequestSockets.js
+++ src/sockets/billRequestSockets.js
@@ -65,7 +65,7 @@ module.exports = (io, db) => {
                 };
                 try {
                     await axios.post("https://webhook.botpress.cloud/6df86dac-9e27-4939-b82d-1b930b382ee6", webhookData);
-                    console.log(`✅ Webhook sent for bill ${bill.bill_client_id}: ${bill.bill_convo_id}: ${bill.bill_request_id}: ${bill.bill_status}`);
+                console.log(`✅ Webhook sent for bill ${bill.client_id}: ${bill.convo_id}: ${bill.bill_request_id}: ${bill.bill_status}`);
                 } catch (error) {
                     console.error("❌ Webhook failed:", error.response?.data || error.message);
                 }
