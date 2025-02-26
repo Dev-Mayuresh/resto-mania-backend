1db20f1f3d4d51ecf72cd4d2a4cf4ad3b55ad048
shantanushok <159703391+shantanushok@users.noreply.github.com>
2025-02-27 00:48:07 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,8 @@ const userController = require('../controllers/userController');
 
 router.post('/register', userController.createUser);  // Use the correct function name
 router.get('/:client_id', userController.getUserById);  // Fetch user details by ID
-router.get('/',userController.getAllUsers);//Fetch all users
+router.get('/',userController.getAllUsers); //Fetch all users
 router.put('/:client_id',userController.updateUser);
+router.post("/update-conversation", userController.updateConversationId); 
 module.exports = router;
 
